47342ef8130a0ed43ffdb09884479a0db7c9b0c1
Alexander Kravchenko <akravchenko188@gmail.com>
2019-10-23 19:12:12 +0300

    sample

--- amf/public/proj/Qt/AMF_test/main.cpp
+++ amf/public/proj/Qt/AMF_test/main.cpp
@@ -16,6 +16,13 @@ int main(int argc, char *argv[])
     amf::AMFComputeFactoryPtr oclComputeFactory;
     context->GetOpenCLComputeFactory(&oclComputeFactory);
 
+    amf::AMFPrograms* pPrograms;
+    factory->GetPrograms(&pPrograms);
+
+    //TODO create real block
+    amf::AMF_KERNEL_ID kernel = 0;
+    const char* kernel_src = "kernel source";
+    pPrograms->RegisterKernelSource(&kernel, L"kernelIDName", "kernelName", strlen(kernel_src), (amf_uint8*)kernel_src, "option");
 
     int deviceCount = oclComputeFactory->GetDeviceCount();
     for(int i = 0; i < deviceCount; ++i)
@@ -26,7 +33,6 @@ int main(int argc, char *argv[])
         amf::AMFComputePtr pCompute;
         pComputeDevice->CreateCompute(nullptr, &pCompute);
 
-        amf::AMF_KERNEL_ID kernel = 0;//tbd
         amf::AMFComputeKernelPtr pKernel;
         pCompute->GetKernel(kernel, &pKernel);
         //pKernel->SetArgInt32(...);
