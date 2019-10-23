20754794446c17dc1260ac7e40010d2384f9a262
Alexander Kravchenko <akravchenko188@gmail.com>
2019-10-23 19:03:56 +0300

    sample

--- amf/public/proj/Qt/AMF_test/main.cpp
+++ amf/public/proj/Qt/AMF_test/main.cpp
@@ -17,7 +17,23 @@ int main(int argc, char *argv[])
     context->GetOpenCLComputeFactory(&oclComputeFactory);
 
 
-    oclComputeFactory;
+    int deviceCount = oclComputeFactory->GetDeviceCount();
+    for(int i = 0; i < deviceCount; ++i)
+    {
+        amf::AMFComputeDevicePtr pComputeDevice;
+        oclComputeFactory->GetDeviceAt(i, &pComputeDevice);
+        pComputeDevice->GetNativeContext();
+        amf::AMFComputePtr pCompute;
+        pComputeDevice->CreateCompute(nullptr, &pCompute);
+
+        amf::AMF_KERNEL_ID kernel = 0;//tbd
+        amf::AMFComputeKernelPtr pKernel;
+        pCompute->GetKernel(kernel, &pKernel);
+        //pKernel->SetArgInt32(...);
+        //pKernel->SetArgBuffer(...);
+        //pKernel->Enqueue(...);
+        //pCompute->FlushQueue();
+    }
 
     return a.exec();
 }
