0694140cfe3d192a1bcf50744a7641d5a74b4009
Ovchinnikov Dmitrii <ovchinnikov.dmitrii@gmail.com>
2020-03-06 11:42:01 +0300

    Some metal samples fixes & metallib build.

--- amf/public/proj/Qt/AMF_test/main_metal.cpp
+++ amf/public/proj/Qt/AMF_test/main_metal.cpp
@@ -1,8 +1,10 @@
 #include "../../../include/core/Factory.h"
 #include "../../../common/AMFFactory.h"
 #include "../../../include/core/Buffer.h"
+#include "iostream"
 
 #define CL_TARGET_OPENCL_VERSION 120
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -28,31 +30,31 @@ int main(int argc, char *argv[])
     AMF_RESULT res;
 
     //amf::AMFComputeDevicePtr pComputeDevice;
-
+cout<<"1"<<endl;
     amf::AMFComputePtr pCompute;
     //pComputeDevice->CreateCompute(nullptr, &pCompute);
     context->GetCompute(amf::AMF_MEMORY_METAL, &pCompute);
 
     amf::AMFComputeKernelPtr pKernel;
     res = pCompute->GetKernel(kernel, &pKernel);
-
+cout<<"2"<<endl;
     amf::AMFBuffer *input = NULL;
     amf::AMFBuffer *output = NULL;
 
     res = context->AllocBuffer(amf::AMF_MEMORY_HOST, 1024 * sizeof(float), &input);
     res = context->AllocBuffer(amf::AMF_MEMORY_METAL, 1024 * sizeof(float), &output);
-
+cout<<"3"<<endl;
     float  *inputData = static_cast<float*>(input->GetNative());
     for (int k = 0; k < 1024; k++)
     {
         inputData[k] = rand() / 50.00;;
     }
 
-    input->Convert(amf::AMF_MEMORY_OPENCL);
-
-    res = pKernel->SetArgBuffer(1, output, amf::AMF_ARGUMENT_ACCESS_WRITE);
+    input->Convert(amf::AMF_MEMORY_METAL);
+cout<<"0"<<endl;
     res = pKernel->SetArgBuffer(0, input, amf::AMF_ARGUMENT_ACCESS_READ);
-
+    res = pKernel->SetArgBuffer(1, output, amf::AMF_ARGUMENT_ACCESS_WRITE);
+cout<<"1"<<endl;
     amf_size sizeLocal[3] = {1024, 0, 0};
     amf_size sizeGlobal[3] = {1024, 0, 0};
     amf_size offset[3] = {0, 0, 0};
@@ -60,8 +62,10 @@ int main(int argc, char *argv[])
     pKernel->GetCompileWorkgroupSize(sizeLocal);
 
     pKernel->Enqueue(1, offset, sizeGlobal, sizeLocal);
+cout<<"2"<<endl;
     pCompute->FlushQueue();
     pCompute->FinishQueue();
+cout<<"3"<<endl;
     output->Convert(amf::AMF_MEMORY_HOST);
     float  *outputData = static_cast<float*>(output->GetNative());
 
---
+++ amf/public/src/core/metal/testWrapper/run.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+build/Test.app/Contents/MacOS/Test -o run
