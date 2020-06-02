47cf8ef84cd86ffca2b8cf4ded2260afda83e7b1
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2020-06-02 17:40:31 +0300

    InitOpenCL from pCommandQueue fix.

--- amf/public/src/core/ContextImpl.cpp
+++ amf/public/src/core/ContextImpl.cpp
@@ -76,14 +76,14 @@ AMF_RESULT AMFContextImpl::InitOpenCL(void *pCommandQueue)
     }
     else
     {
-        cl_context context;
-        cl_device_id deviceId;
-        cl_platform_id platformId;
+        cl_context context = NULL;
+        cl_device_id deviceId = NULL;
+        cl_platform_id platformId = NULL;
 
         size_t resultsize;
-        int err = clGetCommandQueueInfo(cl_command_queue(pCommandQueue), CL_QUEUE_CONTEXT, sizeof(cl_context), context, &resultsize);
-        err |= clGetCommandQueueInfo(cl_command_queue(pCommandQueue), CL_QUEUE_DEVICE, sizeof(cl_device_id), deviceId, &resultsize);
-        err |= clGetDeviceInfo(deviceId, CL_DEVICE_PLATFORM, sizeof(cl_platform_id), platformId, &resultsize);
+        int err = clGetCommandQueueInfo(cl_command_queue(pCommandQueue), CL_QUEUE_CONTEXT, sizeof(cl_context), &context, &resultsize);
+        err |= clGetCommandQueueInfo(cl_command_queue(pCommandQueue), CL_QUEUE_DEVICE, sizeof(cl_device_id), &deviceId, &resultsize);
+        err |= clGetDeviceInfo(deviceId, CL_DEVICE_PLATFORM, sizeof(cl_platform_id), &platformId, &resultsize);
 
 		if (err != CL_SUCCESS)
 		{
