2e7b0695a1957aa34a1f89c426f75717de86bb00
ivm <>
2020-08-13 12:18:08 +0300

    hotfixes for build system

--- amf/public/proj/cmake/generate-static-mac.sh
+++ amf/public/proj/cmake/generate-static-mac.sh
@@ -6,5 +6,5 @@ rm -rf ./mac-gnumake
 mkdir mac-gnumake
 
 cd mac-gnumake
-cmake .. -DOpenCL_INCLUDE_DIR="../../../../../Thirdparty/OpenCL-Headers" -DAMF_CORE_SHARED=1
+cmake .. -DOpenCL_INCLUDE_DIR="../../../../../Thirdparty/OpenCL-Headers" -DAMF_CORE_STATIC=1
 cd ..
