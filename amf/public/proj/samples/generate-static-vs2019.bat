a9417b549cf83592cd13f2ea17cc9d156dc39a8e
Piromancer <Den2000_09@mail.ru>
2020-08-14 12:20:54 +0300

    Submodule update, removed old sample folder

--- amf/public/proj/samples/generate-static-vs2019.bat
+++
@@ -1,7 +0,0 @@
-SETLOCAL
-
-RMDIR /S /Q vs2019
-MKDIR vs2019
-CD vs2019
-
-cmake .. -G "Visual Studio 16 2019" -A x64 -DOpenCL_INCLUDE_DIR="C:\Program Files (x86)\OCL_SDK_Light\include" -DAMF_CORE_STATIC="true"
